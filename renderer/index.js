34ed2d1c8f31d96cf786dce9308eccc5048f4300
Bravy <guibravy@gmail.com>
2016-04-27 23:40:48 -0300

    Build 1.0.1

--- renderer/index.js
+++ renderer/index.js
@@ -1,8 +1,8 @@
 /**
 * @Author: Guilherme Serradilha
 * @Date:   26-Apr-2016, 21:43:17
-* @Last modified by:   Guilherme Seradilha
-* @Last modified time: 27-Apr-2016 17:20:30
+* @Last modified by:   Guilherme Serradilha
+* @Last modified time: 27-Apr-2016, 23:37:58
 */
 
 // prevent dragover
@@ -18,7 +18,8 @@ document.addEventListener('drop',function(event){
 },false);
 
 const admzip = require('adm-zip');
-const fs     = require('fs');
+const fs     = require('fs-extra');
+const moment = require('moment');
 const reader = require('../lib/reader');
 
 //** INITIALIZE ANGULAR ---------------------------------------------------------------------------------------
@@ -34,62 +35,72 @@ var Theme = function($mdThemingProvider) {
 // Controllers
 var ReaderCtrl = function($scope) {
 
+    // load settings
     $scope.config  = require('../config.json');
-    $scope.dirpath = './temp/source';
-    $scope.output = {};
-    //$scope.reader = reader(fs.readFileSync('./temp/Conversa do WhatsApp com Onda Azul.txt', 'utf-8'),
-                           //$scope.config['inputTimestamp'], $scope.config['outputTimestamp']);
 
-    $scope.readDir = function() {
+    /**
+     * Reads all files in given directory
+     * @param  {string} dirPath
+     */
+    $scope.readDir = function(dirPath) {
 
-        var dir = fs.readdirSync($scope.dirpath);
+        var dir = fs.readdirSync(dirPath),
+            output = {};
 
+        // read all files inside dir
         for (var i = 0; i < dir.length; i++) {
+
+            // handle zip file
             if (dir[i].indexOf('.zip') !== -1) {
-                //open zip
-                //extract files
-                var zip = new admzip('./temp' + '/' + dir[i]);
+
+                var zip = new admzip(dirPath + '/' + dir[i]);
+                zip.getEntries().forEach(function(entry) {
+                    if (entry.entryName.indexOf('.txt') !== -1)
+                        output[entry.entryName.replace($scope.config['filenamePrefix'], '').replace('.txt', '')] = reader(zip.readAsText(entry, 'utf8'), $scope.config['inputTimestamp'], $scope.config['outputTimestamp']).getData();
+                    else if ($scope.config['mediaRepository'] !== '')
+                        zip.extractEntryTo(entry, $scope.config['mediaRepository'], false, true);
+                });
+
+            // handle text file
             } else if (dir[i].indexOf('.txt') !== -1) {
-                $scope.output[dir[i].replace($scope.config['filenamePrefix'], '').replace('.txt', '')] = reader(fs.readFileSync($scope.dirpath + '/' + dir[i], 'utf-8'), $scope.config['inputTimestamp'], $scope.config['outputTimestamp']).getData();
+                output[dir[i].replace($scope.config['filenamePrefix'], '').replace('.txt', '')] = reader(fs.readFileSync(dirPath + '/' + dir[i], 'utf-8'), $scope.config['inputTimestamp'], $scope.config['outputTimestamp']).getData();
+
+            // handle media
             } else if ($scope.config['mediaRepository'] !== '') {
-                //move to repo
+                fs.copySync(dirPath + '/' + dir[i], $scope.config['mediaRepository'] + '/' + dir[i]);
             }
         }
 
+        // build csv files
         var csvEvents   = '',
             csvMessages = '',
             addHeader   = true;
 
-        for (var f in $scope.output) {
-            if ($scope.output.hasOwnProperty(f)) {
-                csvEvents   += generateCSV($scope.output[f]['events'], f, addHeader);
-                csvMessages += generateCSV($scope.output[f]['messages'], f, addHeader);
+        for (var f in output) {
+            if (output.hasOwnProperty(f)) {
+                csvEvents   += generateCSV(output[f]['events'],   f, addHeader);
+                csvMessages += generateCSV(output[f]['messages'], f, addHeader);
                 addHeader = false;
             }
         }
 
-        fs.writeFile('./temp/events_w17.csv', csvEvents, 'utf8');
-        fs.writeFile('./temp/messages_w17.csv', csvMessages, 'utf8');
+        // write csv files
+        var currentWeek = moment().format('WW');
+        fs.writeFileSync($scope.config['dataRepository'] + '/events_w'   + currentWeek + '.csv', csvEvents,   'utf8');
+        fs.writeFileSync($scope.config['dataRepository'] + '/messages_w' + currentWeek + '.csv', csvMessages, 'utf8');
     }
-
-    $scope.run = function() {
-        //var files = $scope.reader.getCSV('Onda Azul', true);
-
-        /*for (var f in files) {
-            if (files.hasOwnProperty(f)) {
-                fs.writeFile('./temp/teste_' + f + '.csv', files[f], 'utf8');
-            }
-        }*/
-    };
 
     $scope.saveConfig = function() {
 
     }
 
-    $scope.parseFile = function(buffer) {
-
-    }
-
+    /**
+     * Transforms object-data into an csv
+     * @param  {object[]} obj         - array of JSON objects
+     * @param  {string}   identifier  - identifier column (group name, user, broadcast, etc.)
+     * @param  {boolean}  addHeader   - adds header data
+     * @return {string}
+     */
     var generateCSV = function(obj, identifier, addHeader) {
 
         var csv = '',
@@ -98,6 +109,7 @@ var ReaderCtrl = function($scope) {
         // write headers
         if (addHeader) {
             csv += 'identifier;';
+
             for (var i = 0; i < headers.length; i++)
                 csv += headers[i] + ';';
             csv += '\n';
@@ -106,6 +118,7 @@ var ReaderCtrl = function($scope) {
         // write content
         for (var i = 0; i < obj.length; i++) {
             csv += identifier + ';';
+
             for (var k in obj[i]) {
                 if (obj[i].hasOwnProperty(k)) {
                     csv += obj[i][k] + ';';
